2d6a3748af2eb20d377cecdbd84c32f544196882
Luis Canada <canadaluis5@gmail.com>
2023-07-09 18:50:11 -0400

    Add minimize button

--- src/content/amazon/App.svelte
+++ src/content/amazon/App.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+  import { IconMinus, IconPlus } from '@tabler/icons-svelte';
+  import clsx from 'clsx';
   import { onMount } from 'svelte';
   import { storage } from 'webextension-polyfill';
   import { getCountryOfOrigin, renderProduct as renderProductFlag } from './util';
 
+  let hidden = false;
   let numResolved = 0;
   let productsElms: HTMLDivElement[] = [];
 
@@ -47,22 +50,49 @@
 </script>
 
 <main
-  class="fixed z-[999] bottom-2 right-2 shadow-xl bg-black bg-opacity-40 p-4 text-white flex flex-col gap-4 rounded-lg"
+  class={clsx(
+    'fixed z-[999] bottom-2 right-2 shadow-xl bg-black bg-opacity-40 text-white flex flex-col gap-1 rounded-lg overflow-hidden',
+    hidden && 'h-[55px] w-[55px] p-1',
+    !hidden && 'p-4 h-[200px] w-[300px]'
+  )}
+  style="transition: width 500ms, height 500ms, padding 500ms;"
 >
-  <div class="flex flex-col gap-1">
-    <span>{numResolved} / {productsElms.length} Countries of origin received</span>
-    <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700">
-      <div
-        class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full"
-        style="width: {percentLoaded}"
+  <div class="relative">
+    {#if hidden}
+      <button
+        class="hover:bg-slate-500 bg-opacity-50 transition-all rounded-md p-3"
+        on:click={() => (hidden = false)}
       >
-        {percentLoaded}
-      </div>
-    </div>
+        <IconPlus />
+      </button>
+    {:else}
+      <button
+        class="hover:bg-slate-500 bg-opacity-50 transition-all rounded-md p-3"
+        on:click={() => (hidden = true)}
+      >
+        <IconMinus />
+      </button>
+    {/if}
+
+    <span class="text-lg font-bold">🇺🇸 Countries of Origin 🇨🇳</span>
   </div>
 
-  <button
-    class="bg-blue-400 hover:bg-blue-600 transition-colors rounded-md p-3 bg-opacity-60"
-    on:click={refresh}>Refresh</button
-  >
+  <div class="relative flex flex-col gap-4">
+    <div class="flex flex-col gap-1">
+      <span>{numResolved} / {productsElms.length} Countries of origin received</span>
+      <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+        <div
+          class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full"
+          style="width: {percentLoaded}"
+        >
+          {percentLoaded}
+        </div>
+      </div>
+    </div>
+
+    <button
+      class="bg-blue-400 hover:bg-blue-600 transition-colors rounded-md p-3 bg-opacity-60"
+      on:click={refresh}>Refresh</button
+    >
+  </div>
 </main>
